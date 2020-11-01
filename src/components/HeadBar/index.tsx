b96f50f6abe091c54a82bdc1e2b1fe846f0cd2e3
xiaoheming <504415750@qq.com>
2020-11-01 22:46:15 +0800

    feat: menu bar template

--- src/components/HeadBar/index.tsx
+++ src/components/HeadBar/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import './index.less'
 
-function HeadBar() {
-  return (
-    <div className="head-bar">
-      <div className="bar-wrapper">
-        <div className="menu">
-          <div className="menu-item">
-            <div className="item-text">Import</div>
-          </div>
-          <div className="menu-item">
-            <div className="item-text">Export</div>
+class HeadBar extends React.Component {
+  onBtnClick = (btnType: string) => (e: React.MouseEvent) => {
+    console.log(`${btnType} btn was clicked`)
+  }
+  render() {
+    return (
+      <div className="head-bar">
+        <div className="bar-wrapper">
+          <div className="menu">
+            <div className="menu-item" onClick={this.onBtnClick('import')}>
+              <div className="item-text">Import</div>
+            </div>
+            <div className="menu-item" onClick={this.onBtnClick('export')}>
+              <div className="item-text">Export</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default HeadBar
